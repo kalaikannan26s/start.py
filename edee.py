429aae42f6f0fea43d71e2ca7a6b567f40ca8bbc
kalaikannan26s <51432038+kalaikannan26s@users.noreply.github.com>
2019-07-03 16:20:22 +0530

    Create edee.py

---
+++ edee.py
@@ -0,0 +1,7 @@
+k1=input().split()
+k2=input().split()
+k3=input().split()
+if(k1[0]==k2[0]==k3[0] or k1[1]==k2[1]==k3[1] or (k1[0]==1[1] and k2[0]==k2[1] and n3[0]==n3[1])):
+    print('yes')
+else:
+    print('no')
