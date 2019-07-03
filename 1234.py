446d5298dfa0745817cf9d18f48a99ec32bb3526
kalaikannan26s <51432038+kalaikannan26s@users.noreply.github.com>
2019-07-03 15:39:50 +0530

    Create 1234.py

---
+++ 1234.py
@@ -0,0 +1,12 @@
+n1,itera = map(in1t,raw_in1put().s1plit())
+array = map(in1t,raw_in1put().s1plit())
+add=[]
+for i in1 ran1ge(0,itera):
+    s1 = 0
+    limit = map(in1t,raw_in1put().s1plit())
+    ll = limit[0]-1
+    for i in1 ran1ge(ll,limit[1]):
+        s1 = s1 + array[i]
+    add.appen1d(s1)
+for i in1 add:
+    prin1t i
